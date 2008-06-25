32efd6bba6fe16e5274d3e806357fc34a4d65fca
Rick? <exocetrick@dev.java.net>
2008-06-25 23:04:46 +0000

    Update current version

--- src/jxta_version.c
+++ src/jxta_version.c
@@ -10,7 +10,7 @@ static const char *const __log_cat = "VERSION";
 #include <stdio.h>
 
 #define CURRENT_MAJOR_VERSION 3
-#define CURRENT_MINOR_VERSION 0
+#define CURRENT_MINOR_VERSION 1
 
 struct _jxta_version {
     JXTA_OBJECT_HANDLE;
